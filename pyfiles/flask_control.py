136a554624308c9d7b518aa414a50e3ac7d8b678
Ronet05 <ronet_swaminathan@ymail.com>
2019-03-15 22:29:50 +0530

    Flask app created, connection left, will upload in some time

---
+++ pyfiles/flask_control.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER']="/home/ronet/Documents/Deloitte_Hackathon/pyfiles"
+@app.route('/')
+@app.route('/upload')
+def upload():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['zipfile']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+		
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
+
